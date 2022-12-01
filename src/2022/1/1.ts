d551c7bd46daa365368c85054671f0b48583eb6d
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-12-01 08:12:01 +0100

    feat: add 2022/1 solution

---
+++ src/2022/1/1.ts
@@ -0,0 +1,5 @@
+import { getSums } from "./common.ts";
+
+const result = Math.max(...getSums());
+
+console.log(result);
---
+++ src/2022/1/common.ts
@@ -0,0 +1,11 @@
+import { EOL } from "@/utilities/eol.ts";
+import { getInput } from "@/utilities/get-input.ts";
+
+const input = await getInput(import.meta.url);
+
+export const getSums = () =>
+  input
+    .split(`${EOL}${EOL}`)
+    .map((block) =>
+      block.split(EOL).reduce((sum, value) => sum + Number(value), 0)
+    );
