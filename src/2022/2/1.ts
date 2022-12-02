38f3942d15be24f8cfe1b774d7058ef8501765fd
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-12-02 09:48:36 +0100

    refactor: simplify 2022/2 solution (#3)

--- src/2022/2/1.ts
+++ src/2022/2/1.ts
@@ -1,22 +1,19 @@
 import { sum } from "@/utilities/array.ts";
 
-import {
-  getOutcomeScore,
-  getShapeScore,
-  getStrategyGuide,
-  RESPONSES,
-  SHAPES,
-} from "./common.ts";
+import { getRounds } from "./common.ts";
 
 const result = sum(
-  getStrategyGuide().map(([shape, response]) => {
-    const responseShape = SHAPES[RESPONSES.findIndex((r) => r === response)];
-
-    return getOutcomeScore(
-      shape,
-      responseShape,
-    ) + getShapeScore(responseShape);
-  }),
+  getRounds().map((round) => ({
+    "A X": 1 + 3,
+    "A Y": 2 + 6,
+    "A Z": 3 + 0,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 1 + 6,
+    "C Y": 2 + 0,
+    "C Z": 3 + 3,
+  }[round]!)),
 );
 
 console.log(result);
