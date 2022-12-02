19c97d95641a7b604005a086c70883ccb7eb4ee4
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-12-02 09:54:29 +0100

    style: format code (#4)

--- src/2022/1/2.ts
+++ src/2022/1/2.ts
@@ -2,10 +2,6 @@ import { sum } from "@/utilities/array.ts";
 
 import { getSums } from "./common.ts";
 
-const result = sum(
-  getSums()
-    .sort((a, z) => z - a)
-    .slice(0, 3),
-);
+const result = sum(getSums().sort((a, z) => z - a).slice(0, 3));
 
 console.log(result);
--- src/2022/2/common.ts
+++ src/2022/2/common.ts
@@ -3,6 +3,4 @@ import { getInput } from "@/utilities/get-input.ts";
 
 const input = await getInput(import.meta.url);
 
-export const getRounds = () =>
-  input
-    .split(EOL);
+export const getRounds = () => input.split(EOL);
