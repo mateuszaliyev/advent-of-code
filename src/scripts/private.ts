b74b785f1870d4f6aade74c41560b0a76ca8b4f8
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-12-04 11:03:26 +0100

    refactor: replace `main` function with `try...catch` (#9)

--- src/scripts/private.ts
+++ src/scripts/private.ts
@@ -29,57 +29,52 @@ type Member = {
   stars: number;
 };
 
-const main = async () => {
+try {
   const parsedArguments = parse(Deno.args);
 
   const day = Number(parsedArguments.day);
 
   if (!day || Number.isNaN(day) || day < 1 || day > 25) {
-    log.error(`wrong day: \`${day}\`.`);
-    return;
+    throw new Error(`wrong day: \`${day}\`.`);
   }
 
   const leaderboardId = Number(parsedArguments.leaderboard);
 
   if (!leaderboardId || Number.isNaN(leaderboardId)) {
-    log.error(`wrong leaderboard id: \`${leaderboardId}\`.`);
-    return;
+    throw new Error(`wrong leaderboard id: \`${leaderboardId}\`.`);
   }
 
   const part = Number(parsedArguments.part) as 1 | 2;
 
   if (!part || Number.isNaN(part) || ![1, 2].includes(part)) {
-    log.error(`wrong part: \`${part}\`.`);
-    return;
+    throw new Error(`wrong part: \`${part}\`.`);
   }
 
   let session = parsedArguments.session;
 
   if (!session || typeof session !== "string") {
     log.info(
-      "`--session` argument was not provided; using `SESSION` environment variable instead.",
+      "`--session` argument was not provided; using `SESSION` environment variable instead."
     );
     await import("https://deno.land/std@0.167.0/dotenv/load.ts");
     session = Deno.env.get("SESSION");
   }
 
   if (!session || typeof session !== "string") {
-    log.error("no session given.");
-    return;
+    throw new Error("no session given.");
   }
 
   const response = await fetch(
     `https://adventofcode.com/2022/leaderboard/private/view/${leaderboardId}.json`,
     {
       headers: new Headers([["cookie", `session=${session}`]]),
-    },
+    }
   );
 
   if (!response.ok) {
-    log.error(
-      `response from adventofcode.com finished with a status: ${response.status}.`,
+    throw new Error(
+      `response from adventofcode.com finished with a status: ${response.status}.`
     );
-    return;
   }
 
   let data: Leaderboard;
@@ -87,59 +82,52 @@ const main = async () => {
   try {
     data = await response.json();
   } catch (_error) {
-    log.error(
-      `leaderboard \`${leaderboardId}\` can not be accessed with the provided session.`,
+    throw new Error(
+      `leaderboard \`${leaderboardId}\` can not be accessed with the provided session.`
     );
-    return;
   }
 
   const members = Object.entries(data.members);
 
-  const result = members.filter(([_memberId, member]) =>
-    member.completion_day_level[day]?.[part]?.get_star_ts
-  ).sort(
-    ([_memberAId, memberA], [_memberBId, memberB]) => {
-      const memberATimestamp = memberA.completion_day_level[day]?.[part]
-        ?.get_star_ts;
+  const result = members
+    .filter(
+      ([_memberId, member]) =>
+        member.completion_day_level[day]?.[part]?.get_star_ts
+    )
+    .sort(([_memberAId, memberA], [_memberBId, memberB]) => {
+      const memberATimestamp =
+        memberA.completion_day_level[day]?.[part]?.get_star_ts;
 
-      const memberBTimestamp = memberB.completion_day_level[day]?.[part]
-        ?.get_star_ts;
+      const memberBTimestamp =
+        memberB.completion_day_level[day]?.[part]?.get_star_ts;
 
       if (!memberATimestamp && !memberBTimestamp) {
         return 0;
       }
 
       return memberATimestamp < memberBTimestamp ? -1 : 1;
-    },
-  )
+    })
     .reduce<
       Record<number, { name: string; points: number; timestamp: string }>
-    >(
-      (result, [_memberId, member], index) => {
-        result[index + 1] = {
-          name: member.name,
-          points: Math.min(members.length, 100) - index,
-          timestamp: new Intl.DateTimeFormat("pl-PL", {
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            month: "2-digit",
-            second: "2-digit",
-            year: "numeric",
-          }).format(
-            new Date(member.completion_day_level[day][part].get_star_ts * 1000),
-          ),
-        };
-        return result;
-      },
-      {},
-    );
+    >((result, [_memberId, member], index) => {
+      result[index + 1] = {
+        name: member.name,
+        points: Math.min(members.length, 100) - index,
+        timestamp: new Intl.DateTimeFormat(undefined, {
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          month: "2-digit",
+          second: "2-digit",
+          year: "numeric",
+        }).format(
+          new Date(member.completion_day_level[day][part].get_star_ts * 1000)
+        ),
+      };
+      return result;
+    }, {});
 
   console.table(result);
-};
-
-try {
-  await main();
 } catch (error) {
-  log.error(error);
+  log.error(error.message);
 }
